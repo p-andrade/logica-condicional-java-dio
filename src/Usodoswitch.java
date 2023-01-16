f7e8018679a79984aa962a59788d8da1ad78b953
p-andrade <pedrotmajr@gmail.com>
2023-01-16 13:01:14 -0300

    Atualização do projeto.

---
+++ src/Usodoswitch.java
@@ -0,0 +1,26 @@
+public class Usodoswitch {
+    public static void main(String[] args) {
+        int semana = 1;
+
+        switch (semana){
+            case 1:
+                System.out.println("Certo");
+                break;
+            case 2:
+                System.out.println("Certo");
+                break;
+            case 3:
+                System.out.println("Certo");
+                break;
+            case 4:
+                System.out.println("Errado");
+                break;
+            case 5:
+                    System.out.println("Talvez");
+                    break;
+            default:
+                    System.out.println("Valor indefinido");
+
+        }
+    }
+}
